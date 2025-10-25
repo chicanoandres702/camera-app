679581a0b8aec92dddf56b9743e1a8b7ec6d9226
Demonstratorz <157326583+chicanoandres702@users.noreply.github.com>
2025-10-24 17:16:13 -0700

    Create buggy_script.py

---
+++ buggy_script.py
@@ -0,0 +1,8 @@
+# Triggering the bug-fixing agent on October 24, 2025
+
+def main():
+    # This is a deliberate syntax error
+    print("This script has a bug"
+
+if __name__ == "__main__":
+    main()
